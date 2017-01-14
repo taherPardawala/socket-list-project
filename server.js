529d16cf830374a97e6eff531dadbc060525e0f4
taherPardawala <skypunch2@gmail.com>
2017-01-14 23:55:42 +0530

    Almost done little things left

--- server.js
+++ server.js
@@ -5,9 +5,7 @@ var express = require('express'),
 
 var namelist = []//require('dynamic-array').DynamicArray();
 
-server.listen(3000,()=>{
-    console.log(' localhost:3000');
-});
+server.listen(3000);
 
 app.get('/' , function(req , res){
    res.sendFile(__dirname + '/index.html'); 
@@ -18,10 +16,10 @@ io.sockets.on('connection' , function(socket){
     socket.on('new user' , function(data , callback){
         if(namelist.indexOf(data) != -1){
             callback(false);
-            console.log(data + 'false');
+            //console.log(data + 'false');
         }
         else {
-            console.log(data + 'true');;
+            //console.log(data + 'true');;
             callback(true);
             socket.name = data;
             namelist.push(socket.name)
@@ -33,9 +31,18 @@ io.sockets.on('connection' , function(socket){
      });
     
     socket.on('bought item' , function(data){
-        console.log("REmove item server" + data);
-        socket.broadcast.emit('remove item' , data);
+        //console.log("REmove item server" + data);
+        io.sockets.emit('remove item' , data);
     })
+    
+    socket.on('update chef vis' , function(data){
+        socket.emit('chatbox chef vis' , data);
+    });
+    
+    socket.on('update grocer vis' , function(data){
+        //console.log("Grocer server call");
+        socket.emit('chatbox grocer vis' , data);
+    });
     
     socket.on('disconnect' , function(data){
         if(!socket.name) return;
@@ -43,45 +50,3 @@ io.sockets.on('connection' , function(socket){
         io.sockets.emit('username' , namelist);
     });
 });
-
-/*
-msglist.each(function(item){
-        socket.emit('init chat' , item);
-    });
-    
-    jQuery
-    $(document).ready(function(){
-    $(".deleteMe").on("click", function(){
-       $(this).closest("li").remove(); 
-    });
-});
-
-
-    html
-    <ul>
-        <li>item one <div class='deleteMe'>X</div></li>
-        <li>item two <div class='deleteMe'>X</div></li>
-        <li>item three <div class='deleteMe'>X</div></li>
-        <li>item four <div class='deleteMe'>X</div></li>
-        <li>item five <div class='deleteMe'>X</div></li>
-        <li>item six <div class='deleteMe'>X</div></li>
-        <li>item seven <div class='deleteMe'>X</div></li>
-        <li>item eight <div class='deleteMe'>X</div></li>
-        <li>item nine <div class='deleteMe'>X</div></li>
-    </ul>
-    
-    css
-    ul li{
-        list-style: none;
-        background: gray;
-        margin: 2px;
-        border-raius: 5px;
-        padding: 5px;
-    }
-    
-    .deleteMe{
-        float: right;
-        background: yellow;
-    }
-    jason.stringify
-*/
